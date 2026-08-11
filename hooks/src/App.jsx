083d4308ff94ec4eea9520d60e9baaeaf2f792c8
Ritik <sadhritik6@gmail.com>
2026-08-11 11:58:12 +0530

    added counter

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -5,6 +5,7 @@ function App(){
   let[digit,digitfun]=useState(200);
   let[name,setName]=useState("Harshit");
   let[color,setcolor]=useState("red")
+  let[counter,setcount]=useState(0)
 
   function change(){
     digitfun(400);
@@ -12,18 +13,21 @@ function App(){
   function namefun(){
     setName("Ritik");
   }
-  // function colorfun(){
-  //   setName("Ritik");
-  // }
+  
   return(
     <>
    <div style={{backgroundColor: color}}>
     <h1>This is app page {digit}</h1>
     <button onClick={change}>Change</button>
     <h1>{name}</h1>
+
     <button onClick={namefun}>Change name</button><br />
     <button onClick={()=>setName("priyansh")}>Change name with usestate function</button><br />
-    <button onClick={() => setcolor("pink")}>Change Color</button>
+    <button onClick={() => setcolor("pink")}>Change Color</button><br />
+
+
+    <h1>{counter}</h1>
+    <button onClick={()=>{setcount(counter-1)}}>Decrease</button>   <button onClick={()=>{setcount(counter+1)}}>Increase</button>
     </div>
     </>
   )
