96deca4d04b5ac82085fe28e7c26ab5d0a703df4
Ritik <sadhritik6@gmail.com>
2026-08-14 14:55:35 +0530

    form handling

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./Components/form"
+import Form1 from "./Components/form1"
 function App(){
   
   return(
     <>
     <Form/>
+    <Form1/>
     </>
   )
 }
---
+++ form/src/Components/form1.jsx
@@ -0,0 +1,42 @@
+function Form1() {
+
+    let name;
+    let city;
+
+    function nameData(e) {
+        name = e.target.value;
+    }
+
+    function cityData(e) {
+        city = e.target.value;
+    }
+
+    function frmSubmit(event) {
+        event.preventDefault();
+
+        console.log("Name:", name);
+        console.log("City:", city);
+    }
+
+    return (
+        <>
+            <h1>Form Handling</h1>
+
+            <form onSubmit={frmSubmit}>
+
+                <label>Name</label>
+                <input type="text" onChange={nameData} />
+                <br />
+
+                <label>City</label>
+                <input type="text" onChange={cityData} />
+                <br />
+
+                <input type="submit" />
+
+            </form>
+        </>
+    );
+}
+
+export default Form1;
